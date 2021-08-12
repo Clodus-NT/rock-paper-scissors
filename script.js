ebba926a6e776dda74614138546aed62d5836d55
Clodus-NT <clodus.nt@gmail.com>
2021-08-12 15:14:05 -0700

    CP function is working. Round function is only returning the default string at the moment.

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+let userScore = 0;
+let compScore = 0;
+
 /* This randomly choose a string from an array and
-returns the result.*/
+returns the result. It will be called in the 'round' function. */
 function computerPlayer() {
-    let choicesArray = ["rock", "paper", "scissors"];
+    let choicesArray = ['rock', 'paper', 'scissors'];
 
     return choicesArray[ Math.floor(Math.random() * choicesArray.length) ];
+}
+
+/* This runs 1 round of the game, returns the result, and logs the result to the console. It converts userChoice to lower case so to remove case sensitivity. */
+function round(userChoice, compChoice) {
+    let beginRound = prompt('Best out of five rounds. What\'ll it be?');
+    userChoice = beginRound.toLowerCase();
+
+    compChoice = computerPlayer();
+
+    if ((userChoice==='rock' && compChoice==='rock') ||
+        (userChoice==='paper' && compChoice==='paper') ||
+        (userChoice==='scissors' && compChoice==='scissors')) {
+            return 'It\'s a tie!';
+    } else {
+        return 'It sort of works';
+    }
 }
